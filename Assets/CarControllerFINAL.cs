fe33e3eb00e8dd58b959b0bd7af0731ebaa438e8
Skizzix <shawnsmick@gmail.com>
2020-09-09 19:43:39 -0400

    The Wheel Deal
    Wheels on the junkrat prefab in prison now... do the bouncy thing

--- Assets/CarControllerFINAL.cs
+++ Assets/CarControllerFINAL.cs
@@ -10,6 +10,8 @@ public class CarControllerFINAL : MonoBehaviour
     [SerializeField]
     Transform springBR, springBL, springFL, springFR;
     [SerializeField]
+    Transform wheelBR, wheelBL, wheelFL, wheelFR;
+    [SerializeField]
     private float suspensionForce = 100f;
     [SerializeField]
     private float springDistance = 1f;
@@ -48,18 +50,22 @@ public class CarControllerFINAL : MonoBehaviour
         if (Physics.Raycast(springBL.position,-springBL.up,out bl,springDistance))
         {
             _rb.AddForceAtPosition(springBL.up * (suspensionForce * (1-bl.distance/ springDistance)),springBL.position);
+            wheelBL.localPosition = new Vector3(wheelBL.localPosition.x,-(bl.distance / 2),wheelBL.localPosition.z);
         }
         if (Physics.Raycast(springBR.position,-springBR.up,out br,springDistance))
         {
             _rb.AddForceAtPosition(springBR.up * (suspensionForce * (1 - br.distance / springDistance)),springBR.position);
+            wheelBR.localPosition = new Vector3(wheelBR.localPosition.x,-(br.distance / 2),wheelBR.localPosition.z);
         }
         if (Physics.Raycast(springFL.position,-springBL.up,out fl,springDistance))
         {
             _rb.AddForceAtPosition(springFL.up * (suspensionForce * (1 - fl.distance / springDistance)),springFL.position);
+            wheelFL.localPosition = new Vector3(wheelFL.localPosition.x,-(fl.distance / 2),wheelFL.localPosition.z);
         }
         if (Physics.Raycast(springFR.position,-springFR.up,out fr,springDistance))
         {
             _rb.AddForceAtPosition(springFR.up * (suspensionForce * (1 - fr.distance / springDistance)),springFR.position);
+            wheelFR.localPosition = new Vector3(wheelFR.localPosition.x,-(fr.distance / 2) ,wheelFR.localPosition.z);
         }
     }
 }
