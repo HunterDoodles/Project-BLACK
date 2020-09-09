8fa50bcb585b89caec79ba511a55c38ff549045c
Skizzix <shawnsmick@gmail.com>
2020-09-09 16:53:46 -0400

    Maybe unfucked car physics whatever I'm commiting

---
+++ Assets/CarControllerFINAL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControllerFINAL : MonoBehaviour
+{
+    /*If this shit doesnt end up working I will commit mine <3 -shawn
+    */
+    [SerializeField]
+    Transform springBR, springBL, springFL, springFR;
+    [SerializeField]
+    private float suspensionForce = 100f;
+    [SerializeField]
+    private float springDistance = 1f;
+    [SerializeField]
+    private float accel = 300;
+    [SerializeField]
+    private float turn = 300;
+
+    private Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Suspension();
+        Thrust();//( ͡° ͜ʖ ͡°)
+    }
+
+    private void Thrust()
+    {
+        Vector3 vel = transform.InverseTransformDirection(_rb.velocity);
+        vel = new Vector3(vel.x * .8f,vel.y,vel.z);
+        _rb.velocity = transform.TransformDirection(vel);
+        float inputY = Input.GetAxis("Vertical");
+        _rb.AddRelativeForce(Vector3.forward * accel * inputY);
+        float inputX = Input.GetAxis("Horizontal");
+        _rb.AddRelativeTorque(Vector3.up* turn * inputX);
+    }
+
+    private void Suspension()
+    {
+        RaycastHit br, bl, fl, fr;
+        if (Physics.Raycast(springBL.position,-springBL.up,out bl,springDistance))
+        {
+            _rb.AddForceAtPosition(springBL.up * (suspensionForce * (1-bl.distance/ springDistance)),springBL.position);
+        }
+        if (Physics.Raycast(springBR.position,-springBR.up,out br,springDistance))
+        {
+            _rb.AddForceAtPosition(springBR.up * (suspensionForce * (1 - br.distance / springDistance)),springBR.position);
+        }
+        if (Physics.Raycast(springFL.position,-springBL.up,out fl,springDistance))
+        {
+            _rb.AddForceAtPosition(springFL.up * (suspensionForce * (1 - fl.distance / springDistance)),springFL.position);
+        }
+        if (Physics.Raycast(springFR.position,-springFR.up,out fr,springDistance))
+        {
+            _rb.AddForceAtPosition(springFR.up * (suspensionForce * (1 - fr.distance / springDistance)),springFR.position);
+        }
+    }
+}
--- Assets/Scripts/CarControllerRedux.cs
+++ Assets/Scripts/CarControllerRedux.cs
@@ -6,15 +6,22 @@ public class CarControllerRedux : MonoBehaviour
 {
     //You are going to get some quick and dirty comments for now I'll make it readable later
     //Also all this fucking about and I have <40 lines of code, I hate physics
-    public float speed = 100f;
-    public float maxspeed = 200f;
-    public float rotatespeed = 10f;
-    public float decay = 0.90f; //This value is a artificial drag value that does not interfere with the gravity
+    [SerializeField]
+    private float speed = 100f;
+    [SerializeField]
+    private float maxspeed = 200f;
+    [SerializeField]
+    private float rotatespeed = 10f;
+    [SerializeField]
+    private float decay = 0.90f; //This value is a artificial drag value that does not interfere with the gravity
+    [SerializeField]
+    private Transform CenterOfMass;
     private Rigidbody _rb;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-
+      //  _rb.centerOfMass = CenterOfMass.position;
     }
 
     
@@ -24,7 +31,7 @@ public class CarControllerRedux : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        _rb.velocity=new Vector3(_rb.velocity.x* decay,_rb.velocity.y,_rb.velocity.z * decay);//Drag in the x,z axis.
+       // _rb.velocity=new Vector3(_rb.velocity.x* decay,_rb.velocity.y * decay,_rb.velocity.z * decay);//Drag in the x,z axis.
         
         Vector3 movement = transform.forward *(vertical * speed);
        
@@ -32,6 +39,7 @@ public class CarControllerRedux : MonoBehaviour
         if (Physics.Raycast(transform.position,transform.TransformDirection(Vector3.down),out hit,1.2f))
         {
             _rb.AddForce(movement,ForceMode.VelocityChange); //FM Velocity Change makes the change ignore mass
+            _rb.AddRelativeForce(Vector3.down*5,ForceMode.VelocityChange);
             Vector3 TempVel = Vector3.ClampMagnitude(new Vector3(_rb.velocity.x,0,_rb.velocity.z),maxspeed); //Clamping the max value of speed in the x,z axis this is a hackjob and is terrible though unrelated to the issue with the ramps
             _rb.velocity = new Vector3(TempVel.x,_rb.velocity.y,TempVel.z);//apply the velocity
             //_rb.velocity = movement;
