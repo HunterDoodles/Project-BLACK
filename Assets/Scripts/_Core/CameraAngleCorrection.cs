87d1fd930561d854325e6b49be6396e1f21f3a76
Skizzix <shawnsmick@gmail.com>
2020-09-10 00:10:05 -0400

    Camera Angle Bugfix, and wheel minor change
    Unborked the camera, problem was due to how the rotation angles.
    
    Slightly fixed how the wheels align with the suspension. And actually commented my code HUZZAH.

--- Assets/Scripts/_Core/CameraAngleCorrection.cs
+++ Assets/Scripts/_Core/CameraAngleCorrection.cs
@@ -13,7 +13,10 @@ namespace BLACK.Core
 
         // Variable to Isolate X Axis Rotation of Car
         public float carRotationX;
-
+        [SerializeField]
+        private float cameraUpThresh = 10f;
+        [SerializeField]
+        private float cameraDownThresh = 10f;
         void Update()
         {
 
@@ -21,21 +24,21 @@ namespace BLACK.Core
             // TODO: Fix bug with carRotation <= -15 interacting weirdly with the camera resetting to original
             // Get the X Axis Rotation
             carRotationX = playerCar.transform.eulerAngles.x;
-
+            print(carRotationX);
             // If carRotationX is greater than an angle of 15, camera will shift to help see down hills.        
-            if (carRotationX >= 15)
+            if (carRotationX >= cameraUpThresh && carRotationX<180)
             {
                 playerCamera.transform.localPosition = new Vector3(0, 5.8f, -26f);
                 playerCamera.transform.localEulerAngles = new Vector3(8.9f, 0, 0);
             }
             // Else, if carRotationX is less than an angle of -15, camera will shift to help see up hills.
-            else if (carRotationX <= -15)
+            else if (carRotationX <=360-cameraDownThresh&&carRotationX>=180)
             {
                 playerCamera.transform.localPosition = new Vector3(0, 7.8f, -23.5f);
                 playerCamera.transform.localEulerAngles = new Vector3(15, 0, 0);
             }
             // Else, if carRotation evens back out to around 0-1, camera will shift back to original location.
-            else if (carRotationX > -1 || carRotationX < 1f)
+            else //if (carRotationX > -1 || carRotationX < 1f)
             {
                 playerCamera.transform.localPosition = new Vector3(0, 1, -24f);
                 playerCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
--- Assets/Scripts/_Core/CarControllerFINAL.cs
+++ Assets/Scripts/_Core/CarControllerFINAL.cs
@@ -9,20 +9,24 @@ namespace BLACK.Core
     {
         /*If this shit doesnt end up working I will commit mine <3 -shawn
         */
+        #region Variables 
         [SerializeField]
-        Transform springBR, springBL, springFL, springFR;
+        Transform springBR, springBL, springFL, springFR; //Suspension spring locations
         [SerializeField]
-        Transform wheelBR, wheelBL, wheelFL, wheelFR;
+        Transform wheelBR, wheelBL, wheelFL, wheelFR;//Wheel positions
         [SerializeField]
-        private float suspensionForce = 100f;
+        private float suspensionForce = 100f; //how much force the springs impart when fully compressed
         [SerializeField]
-        private float springDistance = 1f;
+        private float springDistance = 1f;//Length of springs
         [SerializeField]
-        private float accel = 300;
+        private float groundDistance = 3f;//How from the ground the car still does car things
         [SerializeField]
-        private float turn = 300;
+        private float accel = 300;//Force Accel of the car
+        [SerializeField]
+        private float turn = 300;//Turn Torque
+        private Rigidbody _rb;//heh he said Rigid
+        #endregion
 
-        private Rigidbody _rb;
         void Start()
         {
             _rb = GetComponent<Rigidbody>();
@@ -37,37 +41,51 @@ namespace BLACK.Core
 
         private void Thrust()
         {
-            Vector3 vel = transform.InverseTransformDirection(_rb.velocity);
-            vel = new Vector3(vel.x * .8f, vel.y, vel.z);
-            _rb.velocity = transform.TransformDirection(vel);
             float inputY = Input.GetAxis("Vertical");
-            _rb.AddRelativeForce(Vector3.forward * accel * inputY);
             float inputX = Input.GetAxis("Horizontal");
-            _rb.AddRelativeTorque(Vector3.up * turn * inputX);
+
+            //Check to see if the car is more or less touching the ground
+            RaycastHit ground;
+            if (Physics.Raycast(_rb.position,-_rb.transform.up,out ground,groundDistance))
+            {
+                Vector3 vel = transform.InverseTransformDirection(_rb.velocity); //Convert the rigidbodie's world velocity into a local space
+                vel = new Vector3(vel.x * .8f,vel.y,vel.z); //Decay the local sideways velocity
+                _rb.velocity = transform.TransformDirection(vel); //Convert and update local velocity to the world
+                // _rb.AddRelativeForce(ground.transform.forward * accel * inputY);
+                _rb.AddRelativeForce(Vector3.forward * accel * inputY);//Move the car
+
+            }
+            _rb.AddRelativeTorque(Vector3.up * turn * inputX);//Spin the Car
         }
 
         private void Suspension()
         {
+
             RaycastHit br, bl, fl, fr;
+            //Check if the spring is contacting thhe ground
             if (Physics.Raycast(springBL.position, -springBL.up, out bl, springDistance))
             {
+                /*
+                 * Added force at the position of the spring based on how compressed the spring is
+                 * (suspensionForce * (1 - bl.distance / springDistance)) this line is probably jank math
+                */
                 _rb.AddForceAtPosition(springBL.up * (suspensionForce * (1 - bl.distance / springDistance)), springBL.position);
-                wheelBL.localPosition = new Vector3(wheelBL.localPosition.x, -(bl.distance / 2), wheelBL.localPosition.z);
+                wheelBL.localPosition = new Vector3(wheelBL.localPosition.x, -(bl.distance - .25f), wheelBL.localPosition.z); //Makes the wheel match the spring position
             }
             if (Physics.Raycast(springBR.position, -springBR.up, out br, springDistance))
             {
                 _rb.AddForceAtPosition(springBR.up * (suspensionForce * (1 - br.distance / springDistance)), springBR.position);
-                wheelBR.localPosition = new Vector3(wheelBR.localPosition.x, -(br.distance / 2), wheelBR.localPosition.z);
+                wheelBR.localPosition = new Vector3(wheelBR.localPosition.x, -(br.distance - .25f), wheelBR.localPosition.z);
             }
             if (Physics.Raycast(springFL.position, -springBL.up, out fl, springDistance))
             {
                 _rb.AddForceAtPosition(springFL.up * (suspensionForce * (1 - fl.distance / springDistance)), springFL.position);
-                wheelFL.localPosition = new Vector3(wheelFL.localPosition.x, -(fl.distance / 2), wheelFL.localPosition.z);
+                wheelFL.localPosition = new Vector3(wheelFL.localPosition.x, -(fl.distance-.25f ), wheelFL.localPosition.z);
             }
             if (Physics.Raycast(springFR.position, -springFR.up, out fr, springDistance))
             {
                 _rb.AddForceAtPosition(springFR.up * (suspensionForce * (1 - fr.distance / springDistance)), springFR.position);
-                wheelFR.localPosition = new Vector3(wheelFR.localPosition.x, -(fr.distance / 2), wheelFR.localPosition.z);
+                wheelFR.localPosition = new Vector3(wheelFR.localPosition.x, -(fr.distance - .25f), wheelFR.localPosition.z);
             }
         }
     }
