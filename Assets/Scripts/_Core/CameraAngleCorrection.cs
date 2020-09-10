d389f4bada5c08007916084eb54ce1c243430549
Spark <hunterdoodles@gmail.com>
2020-09-09 22:01:36 -0500

    Added Camera Smoothing
    Implemented Camera Smoothing and the beginnings of the camera control system.

---
+++ Assets/Scripts/_Core/CameraAngleCorrection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BLACK.Core
+{
+    public class CameraAngleCorrection : MonoBehaviour
+    {
+
+        // Obtain GameObjects for Transform and Translate Vales
+        public GameObject playerCar;
+        public GameObject playerCamera;
+
+        // Variable to Isolate X Axis Rotation of Car
+        public float carRotationX;
+
+        void Update()
+        {
+
+            // TODO: Smooth out Camera Movement between transforms
+            // TODO: Fix bug with carRotation <= -15 interacting weirdly with the camera resetting to original
+            // Get the X Axis Rotation
+            carRotationX = playerCar.transform.eulerAngles.x;
+
+            // If carRotationX is greater than an angle of 15, camera will shift to help see down hills.        
+            if (carRotationX >= 15)
+            {
+                playerCamera.transform.localPosition = new Vector3(0, 5.8f, -26f);
+                playerCamera.transform.localEulerAngles = new Vector3(8.9f, 0, 0);
+            }
+            // Else, if carRotationX is less than an angle of -15, camera will shift to help see up hills.
+            else if (carRotationX <= -15)
+            {
+                playerCamera.transform.localPosition = new Vector3(0, 7.8f, -23.5f);
+                playerCamera.transform.localEulerAngles = new Vector3(15, 0, 0);
+            }
+            // Else, if carRotation evens back out to around 0-1, camera will shift back to original location.
+            else if (carRotationX > -1 || carRotationX < 1f)
+            {
+                playerCamera.transform.localPosition = new Vector3(0, 1, -24f);
+                playerCamera.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/_Core/StableCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BLACK.Core
+{
+    public class StableCamera : MonoBehaviour
+    {
+
+        // Obtain Car Object
+        public GameObject playerCar;
+        
+        // Isolate Axis Values
+        public float carX;
+        public float carY;
+        public float carZ;
+
+        void Update()
+        {
+            GetCarRotations();
+            // Cancel out carX and carZ movement, to stabilize the camera,
+            // Leaving only the Y axis for moving upwards and downwards.
+            transform.eulerAngles = new Vector3(carX - carX, carY, carZ - carZ);
+        }
+
+       // Get EulerAngle Rotations
+        private void GetCarRotations()
+        {
+            carX = playerCar.transform.eulerAngles.x;
+            carY = playerCar.transform.eulerAngles.y;
+            carZ = playerCar.transform.eulerAngles.z;
+        }
+    }
+}
