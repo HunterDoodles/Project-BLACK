0a66b6f282e4ac4194783dfb97c40fd24c0511dc
Spark <hunterdoodles@gmail.com>
2020-09-11 21:59:22 -0500

    Added not working Car Flipping script
    I hate my life

--- Assets/Scripts/_Core/CarFlipping.cs
+++ Assets/Scripts/_Core/CarFlipping.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarFlipping : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    Rigidbody _rb;
+    public GameObject playerCar;
+    private float timeSinceLastFlipped;
+    private float timeTillUnflipped = 2.5f;
+    public bool isFlipped;
+    public float flippingForce = 1000;
+    public float flippingTime = 2.5f;
+
+    private void Start() 
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        timeSinceLastFlipped = Mathf.Infinity;
+        isFlipped = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        //if car is at certain angle, set to isFlipped
+        if (playerCar.transform.rotation.z >= 90 || playerCar.transform.rotation.z <= 270)
+        {
+            isFlipped = true;
+        }
+        if (isFlipped == true)
+        {
+            timeSinceLastFlipped = Time.time;
+        if (timeSinceLastFlipped >= timeTillUnflipped && isFlipped == true)
+        {
+            isFlipped = false;
+            _rb.AddRelativeTorque(Vector3.forward * flippingForce);
+            return;
+        }
+
     }
+    
+    }
+
 }
