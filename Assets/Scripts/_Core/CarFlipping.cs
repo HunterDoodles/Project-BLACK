cf54ab2523479d9064fa70b5614ac675bbf3c7df
Skizzix <shawnsmick@gmail.com>
2020-09-11 23:55:54 -0400

    Missile and Friends update
    Added the functionality for dumbfire and ground tracking missiles that impart force on things they collide with. Its pretty good. Fixed minor weird issue with springs

--- Assets/Scripts/_Core/CarFlipping.cs
+++ Assets/Scripts/_Core/CarFlipping.cs
@@ -1,46 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CarFlipping : MonoBehaviour
 {
-
-    [SerializeField]
-    Rigidbody _rb;
-    public GameObject playerCar;
-    private float timeSinceLastFlipped;
-    private float timeTillUnflipped = 2.5f;
-    public bool isFlipped;
-    public float flippingForce = 1000;
-    public float flippingTime = 2.5f;
-
-    private void Start() 
+    // Start is called before the first frame update
+    void Start()
     {
-        _rb = GetComponent<Rigidbody>();
-        timeSinceLastFlipped = Mathf.Infinity;
-        isFlipped = false;
+        
     }
 
-    private void Update() 
+    // Update is called once per frame
+    void Update()
     {
-        //if car is at certain angle, set to isFlipped
-        if (playerCar.transform.rotation.z >= 90 || playerCar.transform.rotation.z <= 270)
-        {
-            isFlipped = true;
-        }
-        if (isFlipped == true)
-        {
-            timeSinceLastFlipped = Time.time;
-        if (timeSinceLastFlipped >= timeTillUnflipped && isFlipped == true)
-        {
-            isFlipped = false;
-            _rb.AddRelativeTorque(Vector3.forward * flippingForce);
-            return;
-        }
-
+        
     }
-    
-    }
-
 }
