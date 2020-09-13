7421a9032b4fb2f874598a8ae2d842e59f3eb2b1
Spark <hunterdoodles@gmail.com>
2020-09-12 21:04:56 -0500

    Main Menu Initial Commit
    Added beginnings of our Live Menu system. Implemented Cinemachine and TextMeshPro fully into project.

---
+++ Assets/Scripts/UI/MainMenu/UITransitionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class UITransitionManager : MonoBehaviour
+{
+
+    public CinemachineVirtualCamera currentCamera;
+
+
+    public void Start()
+    {
+        currentCamera.Priority++;
+    }
+
+
+    public void UpdateCamera(CinemachineVirtualCamera target)
+    {
+        currentCamera.Priority--;
+
+        currentCamera = target;
+
+        currentCamera.Priority++;
+    }
+}
