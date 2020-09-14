bd68477eb5f3d630199597f524e75f701451dc88
Spark <hunterdoodles@gmail.com>
2020-09-14 18:59:43 -0500

    Main Menu Re-Commit
    Updated stuff. You know, that's the beef.

---
+++ Assets/Scripts/UI/CharacterSelect/CSelectManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class CSelectManager : MonoBehaviour
+{
+
+    public GameObject[] contestantCams;
+    GameObject currentContestant;
+    int contestantIndex;
+    public Button nextCharacter;
+    public Button previousCharacter;
+
+    public CinemachineVirtualCamera currentCamera;
+
+    void Start() 
+    {     
+        Button next = nextCharacter.GetComponent<Button>();
+        next.onClick.AddListener(TaskOnClickNext);
+
+        Button prev = previousCharacter.GetComponent<Button>();
+        prev.onClick.AddListener(TaskOnClickPrev);
+        
+        contestantIndex = 0;
+        currentContestant = contestantCams[0];
+    }
+
+    void TaskOnClickNext()
+    {
+        Debug.Log("Next character.");
+        contestantIndex++;
+        if (contestantIndex > (contestantCams.Length - 1))
+        {
+            contestantIndex = 0;
+        }
+    }
+
+    void TaskOnClickPrev()
+    {
+        Debug.Log("Previous character.");
+        contestantIndex--;
+        if(contestantIndex < 0)
+        {
+            contestantIndex = contestantCams.Length - 1;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/UI/CharacterSelect/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void DevShellQuickstart()
+    {
+        SceneManager.LoadScene("Prison_Passage_Dev");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("You have selected Quit. In a real build of the game, this will shut down the program");
+        Application.Quit();
+    }
+}
