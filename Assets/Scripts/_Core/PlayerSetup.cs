313687fd88252b4aa463e684eda9b36577a454be
Spark <hunterdoodles@gmail.com>
2020-09-19 22:09:29 -0500

    Radar Initial Commit
    Working on a Radar. Unfinished.

--- Assets/Scripts/_Core/PlayerSetup.cs
+++ Assets/Scripts/_Core/PlayerSetup.cs
@@ -3,6 +3,7 @@ using Mirror;
 
 public class PlayerSetup : NetworkBehaviour
 {
+    // Index to disable any arbitrary amount of scripts that will be needed to allow multiplayer to function properly.
     [SerializeField]
     Behaviour[] componenetsToDisable;
 
@@ -10,6 +11,7 @@ public class PlayerSetup : NetworkBehaviour
 
     void Start()
     {
+        // If you aren't the local player of this object, disable all components that aren't a part of YOUR gameobject in the listed index.
         if (!isLocalPlayer)
         {
             for (int i = 0; i < componenetsToDisable.Length; i++)
@@ -19,6 +21,7 @@ public class PlayerSetup : NetworkBehaviour
         } 
         else
         {
+            // Disables the scene camera (The camera that has an overview of the level before you currently connect to a server)
             sceneCamera = Camera.main;
             if (sceneCamera != null)
             {
